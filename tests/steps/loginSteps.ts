866c0a4f80357970827579b9a4d4a07e2786c1df
divyapentareddy <divya.pentareddy@gmail.com>
2025-06-04 21:44:55 +1000

    more login steps

--- tests/steps/loginSteps.ts
+++ tests/steps/loginSteps.ts
@@ -4,3 +4,18 @@ import {Given, When, Then} from "@cucumber/cucumber"
 Given('I navigate to website', async function () {
            //my test
          });
+
+         When('I click login', async function () {
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
+
+         When('I enter {string} and {string}', async function (string, string2) {
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
+
+         Then('I can see dashboard page', async function () {
+           // Write code here that turns the phrase above into concrete actions
+           return 'pending';
+         });
